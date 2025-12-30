bf7aa3ddfc224a1f61cc8f150486926d74623301
RC2225 <rc2225@ghueder-schrott.com>
2025-12-30 09:22:49 +0800

    update

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,7 +1,5 @@
 
 #include "hello.h"
-#include "simple.h"
-#include "noise.h"
 #include <vector>
 #include <memory>
 
@@ -18,9 +16,18 @@ int main(int argc, char **argv)
         return 1;
     }
     
-    
-    Mat img = imread(filename, IMREAD_COLOR);
-    extractBackground(filename, "/home/timwalter/out.mp4", "/home/timwalter/out.jpg", createBackgroundSubtractorMOG2(500, 16, true));
+    Ptr<BackgroundSubtractorKNN> knn = createBackgroundSubtractorKNN();
+    // Set parameters (or set during creation)
+    knn->setHistory(500);
+    // Number of frames used for background modeling, More = slower adaptation, less noise
+    knn->setDist2Threshold(400.0); // Distance threshold, Lower = more sensitive, higher = less sensitive
+    knn->setDetectShadows(true); // Detect shadows, Detect shadows as gray (127)
+    knn->setShadowValue(127); // Shadow value in mask (gray)
+    knn->setShadowThreshold(0.5); // Shadow threshold (0-1)
 
+    Ptr<BackgroundSubtractor> mog2 = createBackgroundSubtractorMOG2(500, 16, true);
+        
+    Mat img = imread(filename, IMREAD_COLOR);
+    extractBackground(filename, "/home/timwalter/out.mp4", "/home/timwalter/out.jpg", mog2 );
     return 0;
 }
--- cpp/hello.h
+++ cpp/hello.h
@@ -8,138 +8,126 @@
 #include <array>
 #include <functional>
 
-
 using namespace cv;
 using namespace std;
-std::string openFileDialog() {
-    std::array<char, 512> buffer;
-    std::string result;
+std::string openFileDialog()
+{
+	std::array<char, 512> buffer;
+	std::string result;
 
-    FILE* pipe = popen("zenity --file-selection", "r");
-    if (!pipe) return "Error";
+	FILE *pipe = popen("zenity --file-selection", "r");
+	if (!pipe)
+		return "Error";
 
-    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-        result += buffer.data();
-    }
-
-    pclose(pipe);
-
-    // Remove trailing newline character
-    if (!result.empty() && result.back() == '\n') {
-        result.pop_back();
-    }
-
-    return result;
-}
-
-
-void extractBackground(std::string inputVideoPath, std::string outputVideoPath, std::string outputBgImage, Ptr<BackgroundSubtractor> bgSubtractor) {
-	    std::cout << cv::getBuildInformation() << std::endl;
-	    cv::utils::logging::setLogLevel(
-        cv::utils::logging::LOG_LEVEL_VERBOSE
-    );
-
-   
-		VideoCapture cap(inputVideoPath,cv::CAP_FFMPEG);
-		if (!cap.isOpened())
-		{
-			cerr << "Error opening video file!" << endl;
-			std::exit(1);
-		}
-
-		int width = (int)cap.get(CAP_PROP_FRAME_WIDTH);
-		int height = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
-		double fps = cap.get(CAP_PROP_FPS);
-
-		VideoWriter writer(
-			outputVideoPath,
-			VideoWriter::fourcc('m', 'p', '4', 'v'),
-			fps,
-			cv::Size(width, height)
-		);
-
-		
-			
-		// bgSubtractor =
-		// 			cv::bgsegm::createBackgroundSubtractorLSBP(
-		// 			cv::bgsegm::LSBP_CAMERA_MOTION_COMPENSATION_NONE, // Motion compensation
-		// 			20,
-		// 			// Number of samples
-		// 			16,
-		// 			// LSBPattern radius
-		// 			true,
-		// 			// Use color
-		// 			0.5f
-		// 			// Lower threshold
-		// );
-		Mat frame, fgMask, fgMaskClean, foreground;
-
-		// Floating-point background accumulator
-		Mat bgFloat(height, width, CV_32FC3, Scalar(0, 0, 0));
-		Mat bgCount(height, width, CV_32FC1, Scalar(0));
-
-		Mat kernel = getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5));
-
-		while (true)
-		{
-			cap >> frame;
-			if (frame.empty())
-				break;
-
-			// Foreground mask
-			bgSubtractor->apply(frame, fgMask);
-			threshold(fgMask, fgMask, 200, 255, THRESH_BINARY);
-
-			morphologyEx(fgMask, fgMaskClean, MORPH_OPEN, kernel);
-			morphologyEx(fgMaskClean, fgMaskClean, MORPH_DILATE, kernel);
-
-			// Foreground video
-			foreground = Mat::zeros(frame.size(), frame.type());
-			frame.copyTo(foreground, fgMaskClean);
-			writer.write(foreground);
-
-			// Background update mask (background = inverse of fg)
-			Mat bgMask;
-			bitwise_not(fgMaskClean, bgMask);
-
-            Mat frameFloat;
-
-            frame.convertTo(frameFloat, CV_32FC3);
-
-            // Accumulate background
-            frameFloat.copyTo(frameFloat, bgMask);  // zero out foreground
-            bgFloat += frameFloat;
-
-            // Count background samples
-			Mat bgMaskFloat;
-			bgMask.convertTo(bgMaskFloat, CV_32FC1, 1.0 / 255.0);
-			bgCount += bgMaskFloat;
-		}
-
-		cap.release();
-		writer.release();
-		
-		
-
-		// // Final background image
-		// Mat bgFinal;
-		// // divide(bgFloat, bgCount, bgFinal);
-		// Mat bgCount3;
-		// bgCount3.setTo(1, bgCount3 == 0);
-		// cvtColor(bgCount, bgCount3, COLOR_GRAY2BGR);
-
-		// divide(bgFloat, bgCount3, bgFinal);
-
-
-		// // Replace NaNs (never-seen pixels)
-		// bgFinal.setTo(0, bgCount == 0);
-
-		// bgFinal.convertTo(bgFinal, CV_8UC3);
-		// imwrite(outputBgImage, bgFinal);
-
-		cout << "Foreground video saved to: " << outputVideoPath << endl;
-		cout << "Background image saved to: " << outputBgImage << endl;
-
-
+	while (fgets(buffer.data(), buffer.size(), pipe) != nullptr)
+	{
+		result += buffer.data();
 	}
 
+	pclose(pipe);
+
+	// Remove trailing newline character
+	if (!result.empty() && result.back() == '\n')
+	{
+		result.pop_back();
+	}
+
+	return result;
+}
+
+void extractBackground(std::string inputVideoPath, std::string outputVideoPath, std::string outputBgImage, Ptr<BackgroundSubtractor> bgSubtractor)
+{
+	std::cout << cv::getBuildInformation() << std::endl;
+	cv::utils::logging::setLogLevel(
+		cv::utils::logging::LOG_LEVEL_VERBOSE);
+
+	VideoCapture cap(inputVideoPath, cv::CAP_FFMPEG);
+	if (!cap.isOpened())
+	{
+		cerr << "Error opening video file!" << endl;
+		std::exit(1);
+	}
+
+	int width = (int)cap.get(CAP_PROP_FRAME_WIDTH);
+	int height = (int)cap.get(CAP_PROP_FRAME_HEIGHT);
+	double fps = cap.get(CAP_PROP_FPS);
+
+	VideoWriter writer(
+		outputVideoPath,
+		VideoWriter::fourcc('m', 'p', '4', 'v'),
+		fps,
+		cv::Size(width, height));
+
+	// bgSubtractor =
+	// 			cv::bgsegm::createBackgroundSubtractorLSBP(
+	// 			cv::bgsegm::LSBP_CAMERA_MOTION_COMPENSATION_NONE, // Motion compensation
+	// 			20,
+	// 			// Number of samples
+	// 			16,
+	// 			// LSBPattern radius
+	// 			true,
+	// 			// Use color
+	// 			0.5f
+	// 			// Lower threshold
+	// );
+	Mat frame, fgMask, fgMaskClean, foreground;
+
+	// Floating-point background accumulator
+	Mat bgFloat(height, width, CV_32FC3, Scalar(0, 0, 0));
+	Mat bgCount(height, width, CV_32FC1, Scalar(0));
+
+	Mat kernel = getStructuringElement(MORPH_ELLIPSE, cv::Size(5, 5));
+
+	while (true)
+	{
+		cap >> frame;
+		if (frame.empty())
+			break;
+
+		// Foreground mask
+		bgSubtractor->apply(frame, fgMask);
+		threshold(fgMask, fgMask, 200, 255, THRESH_BINARY);
+
+		morphologyEx(fgMask, fgMaskClean, MORPH_OPEN, kernel);
+		morphologyEx(fgMaskClean, fgMaskClean, MORPH_DILATE, kernel);
+
+		// Foreground video
+		foreground = Mat::zeros(frame.size(), frame.type());
+		frame.copyTo(foreground, fgMaskClean);
+		writer.write(foreground);
+
+		// Background update mask (background = inverse of fg)
+		Mat bgMask;
+		bitwise_not(fgMaskClean, bgMask);
+
+		Mat frameFloat, bgMaskFloat;
+
+		frame.convertTo(frameFloat, CV_32FC3);
+
+		frameFloat.copyTo(frameFloat, bgMask); // zero out foreground
+		bgFloat += frameFloat;
+
+		bgMask.convertTo(bgMaskFloat, CV_32FC1, 1.0 / 255.0);
+		bgCount += bgMaskFloat;
+	}
+
+	cap.release();
+	writer.release();
+
+	// Final background image
+	Mat bgFinal;
+	// divide(bgFloat, bgCount, bgFinal);
+	Mat bgCount3;
+	cvtColor(bgCount, bgCount3, COLOR_GRAY2BGR);
+
+	divide(bgFloat, bgCount3, bgFinal);
+
+	// Replace NaNs (never-seen pixels)
+	bgFinal.setTo(0, bgCount == 0);
+
+	bgFinal.convertTo(bgFinal, CV_8UC3);
+	imwrite(outputBgImage, bgFinal);
+
+	cout << "Foreground video saved to: " << outputVideoPath << endl;
+	cout << "Background image saved to: " << outputBgImage << endl;
+}
